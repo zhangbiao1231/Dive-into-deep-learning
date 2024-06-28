6a11279a99e85bffea7544786767f09e51df1be4
Zebulon <44607846+zhangbiao1231@users.noreply.github.com>
2024-06-28 16:56:09 +0800

    Update 线性回归简洁实现.py

--- 动手深度学习/CH3 线性神经网络/3.1 线性回归/线性回归简洁实现.py
+++ 动手深度学习/CH3 线性神经网络/3.1 线性回归/线性回归简洁实现.py
@@ -1,1 +1,17 @@
+"""导入必要的库"""
+import numpy as np
+import torch
+from torch.utils import data
+from d2l import torch as d2l
 
+"""生成y=Xw+b+噪声"""
+true_w = torch.tensor([2, -3.4])
+true_b = 4.2
+features, labels = d2l.synthetic_data(true_w, true_b, 1000)
+
+"""构造一个PyTorch数据迭代器"""
+def load_array(data_arrays, batch_size, is_train=True):  #@save
+    dataset = data.TensorDataset(*data_arrays)
+    return data.DataLoader(dataset, batch_size, shuffle=is_train)
+batch_size = 10
+data_iter = load_array((features, labels), batch_size)
